f26435983f89f1beb7d64f961b8bb471afd01114
FK2469 <FK2469@outlook.com>
2019-01-02 17:49:19 -0500

    Format all jsx files

--- static/javascripts/main.jsx
+++ static/javascripts/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import DragAndDrop from './upload';
+import React from "react";
+import ReactDOM from "react-dom";
+import DragAndDrop from "./upload";
 
 import "../stylesheets/main";
-
 
 main();
 
 function main() {
-    React.render(<DragAndDrop/>, document.getElementById('main'));
+  ReactDOM.render(<DragAndDrop />, document.getElementById("main"));
 }
--- static/javascripts/success.jsx
+++ static/javascripts/success.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import {FileCard} from './card';
+import React from "react";
+import { FileCard } from "./card";
 
-
-var r = window.file_json
-React.render(<FileCard filename={r.filename} size={r.size}
-                time={r.time} type={r.type}
-                url_d={r.url_d} url_p={r.url_p}
-                url_i={r.url_i} url_s={r.url_s}
-                quoteurl={r.quoteurl} />,
-                document.getElementById('main'));
+var r = window.file_json;
+React.render(
+  <FileCard
+    filename={r.filename}
+    size={r.size}
+    time={r.time}
+    type={r.type}
+    url_d={r.url_d}
+    url_p={r.url_p}
+    url_i={r.url_i}
+    url_s={r.url_s}
+    quoteurl={r.quoteurl}
+  />,
+  document.getElementById("main")
+);
